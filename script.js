62ae92ad56350a0271cdb8747cfc5a830e8c3f47
Jon <jonpilarski927@gmail.com>
2021-08-21 19:13:56 -0500

    Added contact section and aligned in bottom

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement('h2');
-h2.textContent = "Software Developer based in Chicago";
+h2.textContent = "Software is Eating the World";
 
 document.querySelector('body').appendChild(h2);
